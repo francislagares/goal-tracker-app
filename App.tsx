30401db84520bada5145fd5b7facfdc62bdee4d6
Francis Lagares <francis.lagares@gmail.com>
2022-04-03 21:42:42 +0200

    style: add styling to list goal item

--- App.tsx
+++ App.tsx
@@ -25,7 +25,9 @@ export default function App() {
       </View>
       <View style={styles.goalsContainer}>
         {courseGoals.map(goal => (
-          <Text key={goal}>{goal}</Text>
+          <View style={styles.goalItem} key={goal}>
+            <Text style={styles.goalText}>{goal}</Text>
+          </View>
         ))}
       </View>
     </View>
@@ -56,5 +58,15 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 4,
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: '#5e0acc',
+    color: 'white',
+  },
+  goalText: {
+    color: 'white',
   },
 });
