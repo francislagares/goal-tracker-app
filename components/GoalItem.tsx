81170474ae289f44fff23429a393f7fe2083a971
Francis Lagares <francis.lagares@gmail.com>
2022-04-06 21:39:29 +0200

    style: add ripple effect & IOs alternative

--- components/GoalItem.tsx
+++ components/GoalItem.tsx
@@ -8,11 +8,15 @@ interface IProps {
 
 export const GoalItem = ({ text, onDeleteGoal, id }: IProps) => {
   return (
-    <Pressable onPress={onDeleteGoal.bind(this, id)}>
-      <View style={styles.goalItem}>
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: '#210644' }}
+        onPress={onDeleteGoal.bind(this, id)}
+        style={({ pressed }) => pressed && styles.pressedItem}
+      >
         <Text style={styles.goalText}>{text}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 };
 
@@ -21,11 +25,14 @@ export default GoalItem;
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: '#5e0acc',
   },
   goalText: {
     color: 'white',
+    padding: 8,
+  },
+  pressedItem: {
+    opacity: 0.5,
   },
 });
